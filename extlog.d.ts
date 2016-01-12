b47c2037d8066fe47aad7fc4a08951098c2b60f7
DomiR <domi.rau@gmail.com>
2016-01-12 14:59:55 +0100

    (feature) Update d.ts

--- extlog.d.ts
+++ extlog.d.ts
@@ -1,16 +1,16 @@
 declare module "extlog" {
 	class ExtLog {
 		constructor(name : string, color: string);
-		
-		debug(title : string, msg? : any);
-		info(title : string, msg? : any);
-		counter(title : string, msg? : any);
-		warning(title : string, msg? : any);
-		error(title : string, msg? : any);
-		fatal(title : string, msg? : any);
-		
+
+		debug(...msg : any[]);
+		info(...msg : any[]);
+		counter(...msg : any[]);
+		warning(...msg : any[]);
+		error(...msg : any[]);
+		fatal(...msg : any[]);
+
 		getCounter(title : string, time : number) : ExtLog.Counter;
-		
+
 		setMinLevel(level : any);
 		static setMinLevel(level : any)
 		static color : any;
